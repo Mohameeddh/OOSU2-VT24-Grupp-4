a4996e170f5171d27a702ea29cdeb23eda0fa063
Mohameeddh <s2106555@student.hb.se>
2024-01-24 17:49:29 +0100

    Eight commit

--- EntitetsLager/VårdPersonal.cs
+++ EntitetsLager/VårdPersonal.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntitetsLager
 {
-    internal class VårdPersonal
+    public class VårdPersonal
     {
+        public int AnställningsNummer { get; set; }
+        public string Namn { get; set; }
+        public string YrkesRoll { get; set; }
+        public int Lösenord {  get; set; }
+        public string Specialisering { get; set; }
     }
 }
