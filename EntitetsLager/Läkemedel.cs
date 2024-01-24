bf81ff63b6dcbe47cc1b4d1b3b288ba5c547233e
Khalid1754 <s2105777@student.hb.se>
2024-01-24 17:46:57 +0100

    Läkemedel/Khalid Commit

---
+++ EntitetsLager/Läkemedel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitetsLager
+{
+    public class Läkemedel
+    {
+        public DateTime UtskrivningsDatum {  get; set; }
+        public string MedicinNamn { get; set; }
+        public string Dosering {  get; set; }
+
+        public Patient PatientNummer { get; set; }
+    }
+}
