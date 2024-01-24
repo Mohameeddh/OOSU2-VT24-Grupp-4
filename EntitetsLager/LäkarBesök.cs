16c0c2ac252d359f25655c91dabfb60b8ce584c6
Mohameeddh <s2106555@student.hb.se>
2024-01-24 20:39:01 +0100

    14th commit

--- EntitetsLager/LäkarBesök.cs
+++ EntitetsLager/LäkarBesök.cs
@@ -15,7 +15,6 @@ namespace EntitetsLager
         public int BesöksNummer { get; set; }
         public DateTime BesöksDatum { get; set; }
         public TimeOnly Tid { get; set; }
-        public int ålder { get; set; }
         public VårdPersonal AnställningsNummer { get; set; }
         public string BesöksSyfte { get; set; }
         public Patient Patienter {  get; set; }
@@ -29,15 +28,6 @@ namespace EntitetsLager
             this.Tid = tid;
             this.AnställningsNummer = anställningsNummer;
             this.BesöksSyfte = besöksSyfte;
-        }
-
-        public LäkarBesök(int besöksNummer, Patient patient, DateTime datum, string besöksSyfte, VårdPersonal anställningsNummer)
-        {
-            BesöksNummer = besöksNummer;
-            this.patient = patient;
-            this.datum = datum;
-            BesöksSyfte = besöksSyfte;
-            AnställningsNummer = anställningsNummer;
         }
     }
 }
