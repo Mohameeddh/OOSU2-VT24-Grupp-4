5db06508aa00e6209364140e88c58f296c09a8f4
Khalid1754 <s2105777@student.hb.se>
2024-01-24 18:17:49 +0100

    13th Commit

--- EntitetsLager/LäkarBesök.cs
+++ EntitetsLager/LäkarBesök.cs
@@ -8,6 +8,9 @@ namespace EntitetsLager
 {
     public class LäkarBesök
     {
+        private Patient patient;
+        private DateTime datum;
+
         public Patient PatientNummer { get; set; }
         public int BesöksNummer { get; set; }
         public DateTime BesöksDatum { get; set; }
@@ -26,6 +29,15 @@ namespace EntitetsLager
             this.Tid = tid;
             this.AnställningsNummer = anställningsNummer;
             this.BesöksSyfte = besöksSyfte;
+        }
+
+        public LäkarBesök(int besöksNummer, Patient patient, DateTime datum, string besöksSyfte, VårdPersonal anställningsNummer)
+        {
+            BesöksNummer = besöksNummer;
+            this.patient = patient;
+            this.datum = datum;
+            BesöksSyfte = besöksSyfte;
+            AnställningsNummer = anställningsNummer;
         }
     }
 }
