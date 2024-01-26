4e9d1d8c54f7cf929118b25f667f2d3033c55178
Mohameeddh <s2106555@student.hb.se>
2024-01-26 18:42:34 +0100

    31th commit

--- Presentationslager/Program.cs
+++ Presentationslager/Program.cs
@@ -76,6 +76,7 @@ namespace Presentationslager
         private static void HuvudMeny()
         {
             Console.WriteLine("----HuvudMeny----");
+            Console.WriteLine("FUNKAR DET????");
             Console.ReadLine();
         }
     }
