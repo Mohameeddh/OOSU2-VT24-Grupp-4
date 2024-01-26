d7828b64845318f3cc49090e9db1c56e358f1f57
Mohameeddh <s2106555@student.hb.se>
2024-01-26 16:33:54 +0100

    25th commit

--- AffärsLager/Kontroller.cs
+++ AffärsLager/Kontroller.cs
@@ -10,6 +10,20 @@ namespace AffärsLager
 {
     public class Kontroller
     {
+        private UnitOfWork unitOfWork;
+        public LäkarBesök BokaBesök(int patient, int besöksNummer, DateTime datum, TimeSpan tid, int anställningsNummer, string besöksSyfte)
+        {
+            DateTime dateTime = DateTime.Now.AddDays(12);
+            LäkarBesök besök = new LäkarBesök(patient, besöksNummer, datum, tid, anställningsNummer, besöksSyfte);
+            unitOfWork.LäkarBesökRepository.Add(besök);
+            unitOfWork.Save();
+            return besök;
+        }
 
+        public Patient HämtaPatient(int  patientNummer) 
+        {
+            Patient patient = unitOfWork.PatientRepository.FirstOrDefault(p => p.PatientNummer == patientNummer);
+            return patient;
+        }
     }
 }
