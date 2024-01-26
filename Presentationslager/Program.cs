bda5188223c8e3359c918595973c9d6e7b3de4e2
Mohameeddh <s2106555@student.hb.se>
2024-01-26 15:43:16 +0100

    ReadLine commit

--- Presentationslager/Program.cs
+++ Presentationslager/Program.cs
@@ -11,7 +11,7 @@ namespace Presentationslager
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Daniel");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
