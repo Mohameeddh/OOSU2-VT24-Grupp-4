bda33a9c31bf6be654218e1607c4280dd4ab7582
a-bakar1 <s2207240@student.hb.se>
2024-01-26 19:38:14 +0100

    uppdaterad

--- Presentationslager/Program.cs
+++ Presentationslager/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -76,10 +77,50 @@ namespace Presentationslager
 
         private static void HuvudMeny()
         {
+            bool val = false;
+            while (!val)
             Console.WriteLine("----HuvudMeny----");
-            Console.Write("Tryck 1 för att boka ett nytt läkarbesök:");
-            Console.WriteLine("Tryck 2 ");
-            Console.ReadLine();
+            Console.WriteLine("Tryck 1 för att boka ett nytt läkarbesök");
+            Console.WriteLine("Tryck 2 för att hantera läkarbesök");
+            Console.WriteLine("Tryck 3 för att registrera patientuppgift");
+            Console.WriteLine("Tryck 4 för att uppdatera patientuppgift");
+            Console.WriteLine("Tryck 5 för att avsluta programmet");
+
+            Console.Write("Välj ett av de alternativ ovanför: ");
+            string val = Console.ReadLine();
+            while (val != null) { }
+            switch(val)
+            {
+                case "1":
+                    BokaLäkarbesök();
+                    break;
+
+                case "2":
+                    HanteraLäkarbesök();
+                    break;
+
+                case "3":
+                    RegistreraPatientuppgift();
+                    break;
+
+                case "4":
+                    UppdateraPatientuppgift();
+                    break;
+
+                case "5":
+                    AvslutaProgram();
+                    break;
+
+                default:
+                    Console.WriteLine("Ogiltig val, försök igen!");
+                    break;
+
+                
+               
+                    
+
+                
+            }
         }
     }
 }
